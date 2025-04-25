8ceb4ab5c2b1db16e36f3f9acb5ef17d1018204a
Madalyn Redding <mredding@microsoft.com>
2025-04-25 14:42:53 -0700

    first commit

---
+++ AsyncLibrary/AsyncPageable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncLibrary
+{
+    public class AsyncPageable<T> : IAsyncEnumerable<T>
+    {
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AsyncLibrary/CollectionClass.cs
@@ -0,0 +1,16 @@
+﻿
+namespace AsyncLibrary
+{
+    public class CollectionClass : IAsyncEnumerable<object>
+    {
+        public AsyncPageable<object> GetAllAsync()
+        {
+            return new AsyncPageable<object>();
+        }
+
+        IAsyncEnumerator<object> IAsyncEnumerable<object>.GetAsyncEnumerator(CancellationToken cancellationToken)
+        {
+            return GetAllAsync().GetAsyncEnumerator(cancellationToken);
+        }
+    }
+}
